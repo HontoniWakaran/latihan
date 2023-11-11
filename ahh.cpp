6fb4ac53c578942c7f3c35bd454d1617e2d1c235
HontoniWakaran <150516406+HontoniWakaran@users.noreply.github.com>
2023-11-11 17:14:19 +0700

    Add files via upload

---
+++ ahh.cpp
@@ -0,0 +1,37 @@
+//using namespace std;
+
+//int main(){
+//int x,y,z,m,n,m2;
+//float m2;
+//x = 9+4; /* menghitung 9+4 */
+//y = 9-4; /* menghitung 9-4 */
+//z = 9*4; /* menghitung 9*4 */
+//m = 9/4; /* menghitung 9/4 */
+//m2 = 9.0/4.0; /* menghitung */
+//n = 9%4; /* menghitung */
+//clrscr() ;
+//cout << "nilai dari 9+4 = " << X;
+//out << "\nnilai dari 9-4 = " << x;
+//cout << "\nnilai dari 9*4 = " << x;
+//cout << "\nnilai dari 9/4 = " << x;
+//cout << "\nnilai dari 9 mod 4 = " << x;
+//cout << "\nnilai dari (9*0)/(4*0) = " << x;
+return 0;
+}
+
+using namespace std;
+
+int main (){
+	int x,y,z,m,n
+	float m2
+	x = -9+4;
+	y = -9-(-4);
+	z = -9*4;
+	m = 11/-4;
+	m2 = 9.0/4.0;
+	n = 9
+	cout }
+	
+	
+	
+
